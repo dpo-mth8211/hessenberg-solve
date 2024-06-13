f203d2e0dfc75f88f83bccc5d7f1928e86d13e52
Jean Marie Vilaire <jeanmarievilaire@gmail.com>
2024-06-13 19:20:34 -0400

    Modifications Apportees a exercice.jl

--- exercice.jl
+++ exercice.jl
@@ -6,10 +6,21 @@ using LinearAlgebra
 function backsolve(R::UpperTriangular, b)
   x = similar(b)
   ### votre code ici ; ne rien modifier d'autre
-  # ...
-  ###
+  n = length(b)
+  for i in n:-1:1
+      if R[i, i] == 0
+        println("Matrice non inversible infinite de solutions")
+      end
+      x[i] = b[i]
+      for j in i+1:n
+          x[i] -= R[i, j] * x[j]
+      end
+      x[i] /= R[i, i]
+  end
   return x
 end
+  ###
+  
 
 # 2. Modifiez la fonction suivante pour qu'elle renvoie la solution x
 #    du système Hessenberg supérieur Hx = b à l'aide de rotations ou
@@ -20,8 +31,25 @@ end
 #    Seul le cas réel sera testé ; pas le cas complexe.
 function hessenberg_solve(H::UpperHessenberg, b)
   ### votre code ici ; ne rien modifier d'autre
+  n = size(H, 1)
+  
+  for j in 1:n-1
+      for i in j+1:n
+          if H[i, j] != 0
+              r = sqrt(H[j, j]^2 + H[i, j]^2)
+              c = H[j, j] / r
+              s = H[i, j] / r
+              
+              for k in j:n
+                  H[j, k], H[i, k] = c * H[j, k] + s * H[i, k], -s * H[j, k] + c * H[i, k]
+              end
+              
+              b[j], b[i] = c * b[j] + s * b[i], -s * b[j] + c * b[i]
+          end
+      end
+  end
   # ...
-  # x = ...
+  x = backsolve(UpperTriangular(H), b)
   ###
   return x
 end
