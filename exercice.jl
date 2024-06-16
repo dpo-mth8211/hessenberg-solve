f39993b2e39b4a5372ffd3075c13d30c3ab9e456
Harkdera <hariderarako@gmail.com>
2024-06-16 13:55:15 -0400

    backsolve d'une matrice R et resolution d'une matirce Hessemberg en utilisant des rotations de Givens

--- exercice.jl
+++ exercice.jl
@@ -4,10 +4,10 @@ using LinearAlgebra
 #    du système triangulaire supérieur Rx = b.
 #    Votre fonction ne doit modifier ni R ni b.
 function backsolve(R::UpperTriangular, b)
-  x = similar(b)
-  m=size(R,1)
- for i=m:-1:1
-  x[i] = (b[i] - dot(R[i, i+1:end], x[i+1:end])) / R[i, i]
+  x = similar(b)  # création d'un vecteur de la meme taille que b
+  m=size(R,1)     # nombre de ligne et de colonne de R pour Un R carree
+ for i=m:-1:1     # loop permettant de navigué les lignes de R commençant de la derniere ligne à la première 
+  x[i] = (b[i] - dot(R[i, i+1:end], x[i+1:end])) / R[i, i]   # Bacskolve implementation 
  end 
   return x
 end
@@ -20,10 +20,30 @@ end
 #    fonction ne doit pas les renvoyer.
 #    Seul le cas réel sera testé ; pas le cas complexe.
 function hessenberg_solve(H::UpperHessenberg, b)
-  ### votre code ici ; ne rien modifier d'autre
-  # ...
-  # x = ...
-  ###
+  m=size(H,1)
+  for i=1:m-1    
+      j=i+1
+    ########Calcul des parametres de la matrice de rotation############
+      r=norm(H[:,i])
+      c=H[i,i]/r
+      s=H[j,i]/r
+    ########Appliquer les rotations de givens tout en s'assurant de modifier seulement les parametres non nuls 
+       for k = i:m 
+        Hik = c * H[i, k] + s * H[j, k]  
+        Hjk = -s * H[i, k] + c * H[j, k]
+        H[i, k] = Hik
+        H[j, k] = Hjk
+      end
+    ##########Modification de la matrice B############################################
+    bi = c * b[i] + s * b[j]
+    bj = -s * b[i] + c * b[j]
+    b[i] = bi
+    b[j] = bj
+  end 
+  ############Compatibilité avec la fonction backsolve##############
+    R=UpperTriangular(H)
+  ############Résolution en utilisant la fonction backsolve#########
+  x = backsolve(R, b)
   return x
 end
 
