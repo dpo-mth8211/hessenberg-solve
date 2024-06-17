391cd4f90817b782147337bd184b3c2f79402e26
Jean Marie Vilaire <jeanmarievilaire@gmail.com>
2024-06-17 00:47:37 -0400

    Modifications Apportees a exercice.jl

--- exercice.jl
+++ exercice.jl
@@ -3,14 +3,20 @@ using LinearAlgebra
 # 1. Modifiez la fonction suivante pour qu'elle renvoie la solution x
 #    du système triangulaire supérieur Rx = b.
 #    Votre fonction ne doit modifier ni R ni b.
-function backsolve(R::UpperTriangular, b)
-  x = similar(b)
-  ### votre code ici ; ne rien modifier d'autre
-  # ...
-  ###
-  return x
-end
 
+function backsolve(R::UpperTriangular, b)
+
+    x = similar(b)
+    n = length(b)
+    
+    for i in n:-1:1
+        x[i] = (b[i] - dot(R[i, i+1:end], x[i+1:end])) / R[i, i]
+    end
+    return x
+end
+###
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
 # 2. Modifiez la fonction suivante pour qu'elle renvoie la solution x
 #    du système Hessenberg supérieur Hx = b à l'aide de rotations ou
 #    réflexions de Givens et d'une remontée triangulaire.
@@ -18,17 +24,33 @@ end
 #    Il n'est pas nécessaire de garder les rotations en mémoire et la
 #    fonction ne doit pas les renvoyer.
 #    Seul le cas réel sera testé ; pas le cas complexe.
-function hessenberg_solve(H::UpperHessenberg, b)
-  ### votre code ici ; ne rien modifier d'autre
-  # ...
-  # x = ...
-  ###
-  return x
-end
 
+#function hessenberg_solve(H::UpperHessenberg{Float64, Matrix{Float64}}, b::Vector{Float64})
+function hessenberg_solve(H::UpperHessenberg, b)
+
+    _,n = size(H)
+    for i in 1:n-1
+        r = sqrt.(H[i,i]^2 + H[i+1,i]^2)
+        c = H[i,i] / r
+        s = H[i+1,i] / r
+
+        H[i:i+1,i:end] .= [c s; -s c] * H[i:i+1,i:end]#appliquer Givens rotation sur H
+        b[i:i+1] .= [c s; -s c] * b[i:i+1]#appliquer Givens rotation sur b
+
+    end
+    # H est maintenat triangulaire superieure on peut appeler la fonction backsolve
+    #x = backsolve(UpperTriangular(H), b)
+    x = similar(b)
+    x = backsolve(H, b)
+    return x
+end
+###
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
 # vérification
 using Test
 for n ∈ (10, 20, 30)
+#for n ∈ (10, 20, 30, 40, 50, 60)#enlever apres
   A = rand(n, n)
   b = rand(n)
   R = UpperTriangular(A)
@@ -37,4 +59,10 @@ for n ∈ (10, 20, 30)
   H = UpperHessenberg(A)
   x = hessenberg_solve(copy(H), copy(b))
   @test norm(H * x - b) ≤ sqrt(eps()) * norm(b)
+
+  if norm(H * x - b) ≤ sqrt(eps()) * norm(b)#enlever a la remise
+    println("Test Passed")#enlever a ala remis
+  end
+
 end
+
